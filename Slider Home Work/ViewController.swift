80654099db42f4164d6695e84994b53ca148da8b
1 <1>
2023-05-03 11:06:02 +0300

    Initial Commit

---
+++ Slider Home Work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider Home Work
+//
+//  Created by Анатолий Сидин on 03.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
