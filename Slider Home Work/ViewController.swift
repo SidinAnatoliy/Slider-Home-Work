d5bd7e8da55163c29ad70007e29db03615701cb6
1 <1>
2023-05-03 13:11:18 +0300

    finished project

--- Slider Home Work/ViewController.swift
+++ Slider Home Work/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = CGFloat(5)
+        setupSlider()
+        setupLabel()
+        
     }
 
-
+    @IBAction func sliderAction() {
+        setupSlider()
+        setupLabel()
+    
+    }
+    
+    private func setupSlider() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    
+    private func setupLabel() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        
+    }
 }
 
